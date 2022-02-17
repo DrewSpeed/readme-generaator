08f809208d42711dbe8405fa35ec234931da2e30
Drew Speed <drewjspeed@gmail.com>
2022-02-17 09:06:50 -0600

    added section titles and started inquirer questions

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        validate: function (response) {
+            if (!response.length) {
+                return console.log("Please enter a valid title name.")
+            }
+            console.log(response);
+            return true;
+        }
+    },
+]);
+};
+questions()
+    .then(answers => {
+        console.log(answers);
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
